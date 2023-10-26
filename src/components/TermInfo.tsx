0fc65168b70915b3b9666cc0d4525209995fc264
MrPeterJin <Cheng.Jin@std.uestc.edu.cn>
2023-10-26 13:48:58 +0800

    Fixed redirect

--- src/components/TermInfo.tsx
+++ src/components/TermInfo.tsx
@@ -3,7 +3,7 @@ import { User, WebsiteName, Wrapper } from "./styles/TerminalInfo.styled";
 const TermInfo = () => {
   return (
     <Wrapper>
-      <User>visitor</User>@<WebsiteName>terminal.petergam.top</WebsiteName>:~$
+      <User>visitor</User>@<WebsiteName>petergam.top</WebsiteName>:~$
     </Wrapper>
   );
 };
